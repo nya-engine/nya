04b89c396c01beef57e0bba2c90f6cc4c21c1608
unn4m3d <smelnikov871@gmail.com>
2017-03-07 12:41:32 +1000

    Input

--- src/nya_engine/input/input.cr
+++ src/nya_engine/input/input.cr
@@ -2,14 +2,31 @@ require "./events"
 
 module Nya
   module Input
-    @@keymap = Hash(Int32, Bool).new
+    @@keymap = Hash(Keycode, Bool).new
 
-    Nya::Event.subscribe_typed :key_up , as: KeyboardEvent do |evt|
+    Nya::Event.subscribe_typed :key_up, as: KeyboardEvent do |evt|
       if evt.nil?
         Nya.log.debug "KeyUp hook received Non-keyboard event", "Input"
       else
-        #puts event.inner.keysym.scancode.to_s
+        @@keymap[evt.keycode] = false if @@keymap.has_key? evt.keycode
       end
+    end
+
+    Nya::Event.subscribe_typed :key_down, as: KeyboardEvent do |evt|
+      if evt.nil?
+        Nya.log.debug "KeyDown received Non-keyboard event"
+      else
+        @@keymap[evt.keycode] = true
+      end
+    end
+
+    def self.key?(kcode : Keycode)
+      @@keymap[kcode]
+    end
+
+    def self.key?(kcode)
+      kc = Keycode.from_value(kcode.to_s.upcase)
+      key? kc
     end
   end
 end
