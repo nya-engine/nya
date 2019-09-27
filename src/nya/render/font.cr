9977771e7641f2fc75894a40ff5f15234bb7c8b1
unn4m3d <smelnikov871@gmail.com>
2019-09-27 14:40:32 +1000

    Loading fonts via FontConfig

---
+++ src/nya/render/font.cr
@@ -0,0 +1,21 @@
+require "../bindings/fontconfig"
+
+module Nya::Render
+  class Font < Component
+    property src = ""
+    attribute src : String
+
+    property awaken = false
+
+    def awake
+      return if @awaken
+
+      config = LibFontConfig.config_get_current
+      result = LibFontConfig.config_app_add_font_file(config, @src)
+
+      log.error "Cannot load #{@src}", "Font" if result == 0
+
+      @awaken = true
+    end
+  end
+end
